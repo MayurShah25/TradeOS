b6afa4d32dc8d48f44548a353c77362f32c113d7
MayurShah25 <shamay25@gmail.com>
2026-09-02 14:31:55 +0530

    feat: export execution authorization boundary

--- tradeos/execution/__init__.py
+++ tradeos/execution/__init__.py
@@ -1,11 +1,23 @@
 """Paper-safe execution boundary for TradeOS."""
 
+from .authorization import (
+    AuthorizationLedger,
+    AuthorizationStatus,
+    ExecutionAuthorization,
+    ExecutionAuthorizationPolicy,
+    OperatingMode,
+)
 from .models import Order, OrderSide, OrderStatus, OrderType
 from .paper_broker import PaperBroker
 from .ports import BrokerExecutionPort
 
 __all__ = [
+    "AuthorizationLedger",
+    "AuthorizationStatus",
     "BrokerExecutionPort",
+    "ExecutionAuthorization",
+    "ExecutionAuthorizationPolicy",
+    "OperatingMode",
     "Order",
     "OrderSide",
     "OrderStatus",
