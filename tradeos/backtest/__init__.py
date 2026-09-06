bd65902a26fd5700f0685923f5ecc88b841c7c67
MayurShah25 <shamay25@gmail.com>
2026-09-07 04:21:57 +0530

    feat(backtest): export walk-forward analytics

--- tradeos/backtest/__init__.py
+++ tradeos/backtest/__init__.py
@@ -5,6 +5,7 @@ from .engine import BacktestEngine
 from .robustness import RobustnessAnalyzer, RobustnessResult, RobustnessScenario
 from .types import BacktestRequest, BacktestResult, BacktestTrade, ExecutionCostModel
 from .walk_forward import WalkForwardFold, WalkForwardRequest, WalkForwardValidator
+from .walk_forward_analytics import WalkForwardMetrics, calculate_walk_forward_metrics
 
 __all__ = [
     "BacktestEngine",
@@ -17,7 +18,9 @@ __all__ = [
     "RobustnessResult",
     "RobustnessScenario",
     "WalkForwardFold",
+    "WalkForwardMetrics",
     "WalkForwardRequest",
     "WalkForwardValidator",
     "calculate_metrics",
+    "calculate_walk_forward_metrics",
 ]
