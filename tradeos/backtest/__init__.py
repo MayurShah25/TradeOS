2c3ee92465b217d316c1705ae0bdec0fc4458d95
MayurShah25 <shamay25@gmail.com>
2026-09-06 19:45:50 +0530

    feat(backtest): export robustness primitives

--- tradeos/backtest/__init__.py
+++ tradeos/backtest/__init__.py
@@ -2,6 +2,7 @@
 
 from .analytics import BacktestMetrics, calculate_metrics
 from .engine import BacktestEngine
+from .robustness import RobustnessAnalyzer, RobustnessResult, RobustnessScenario
 from .types import BacktestRequest, BacktestResult, BacktestTrade, ExecutionCostModel
 
 __all__ = [
@@ -11,5 +12,8 @@ __all__ = [
     "BacktestResult",
     "BacktestTrade",
     "ExecutionCostModel",
+    "RobustnessAnalyzer",
+    "RobustnessResult",
+    "RobustnessScenario",
     "calculate_metrics",
 ]
